9f293d214fd856d9d07f0ef9b008e58b0f438b37
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-05-03 10:10:10 +0400

    Добавляет рефакторинг getRandomInt

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports.getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  const intMin = Math.ceil(min);
+  const intMax = Math.floor(max);
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (intMax - intMin + 1)) + intMin;
 };
 
 module.exports.shuffle = (someArray) => {
