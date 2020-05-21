c6900b01e7cbfc07e2f2b82279b922025d3d5a54
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-05-21 20:06:27 +0400

    Добавляет шаблоны страниц

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,9 +4,9 @@ const { Router } = require(`express`);
 
 const router = new Router();
 
-router.get(`/`, (req, res) => res.send(`/`))
-router.get(`/register`, (req, res) => res.send(`/register`));
-router.get(`/login`, (req, res) => res.send(`/login`));
-router.get(`/search`, (req, res) => res.send(`/search`));
+router.get(`/`, (req, res) => res.render(`main/main`));
+router.get(`/register`, (req, res) => res.render(`main/sign-up`));
+router.get(`/login`, (req, res) => res.render(`main/login`));
+router.get(`/search`, (req, res) => res.render(`main/search-result`));
 
 module.exports = router;
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,7 +4,7 @@ const { Router } = require(`express`);
 
 const router = new Router();
 
-router.get(`/`, (req, res) => res.send(`/my`));
-router.get(`/comments`, (req, res) => res.send(`/my/comments`));
+router.get(`/`, (req, res) => res.render(`my/my-tickets`));
+router.get(`/comments`, (req, res) => res.render(`my/comments`));
 
 module.exports = router;
