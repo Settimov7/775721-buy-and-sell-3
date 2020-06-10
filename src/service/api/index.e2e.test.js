74f5656a953ea863674d79e892c491c1c851a747
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-06-10 14:51:47 +0400

    Добавляет тесты для проверки маршрутов для работы с категориями

--- src/service/api/index.e2e.test.js
+++ src/service/api/index.e2e.test.js
@@ -589,6 +589,74 @@ describe(`Offers API end-points`, () => {
       expect(res.body).not.toContainEqual(mockComment2);
     });
   });
+
+  describe(`GET api/categories`, () => {
+    it(`should return empty array if no offers`, async () => {
+      const server = await createServer([]);
+
+      const res = await request(server).get(`/api/categories`);
+
+      expect(res.body).toEqual([]);
+    });
+
+    it(`should return ["Животные", "Посуда"]`, async () => {
+      const mockOffer1 = {
+        id: 'k2MJRx',
+        category: ['Животные'],
+        description: 'Если найдёте дешевле — сброшу цену. Бонусом отдам все аксессуары. Даю недельную гарантию. Продаю с болью в сердце...',
+        picture: 'item12.jpg',
+        title: 'Продам отличную подборку фильмов на VHS',
+        type: 'sale',
+        sum: 25913,
+        comments: [],
+      };
+      const mockOffer2 = {
+        id: 'bDBSaq',
+        category: ['Посуда'],
+        description: 'Две страницы заляпаны свежим кофе. Даю недельную гарантию. Если найдёте дешевле — сброшу цену.',
+        picture: 'item13.jpg',
+        title: 'Подам отличную подборку фильмов на VHS',
+        type: 'offer',
+        sum: 31230,
+        comments: [],
+      };
+      const mockOffers = [mockOffer1, mockOffer2];
+      const server = await createServer(mockOffers);
+
+      const res = await request(server).get(`/api/categories`);
+
+      expect(res.body).toEqual(['Животные', 'Посуда']);
+    });
+
+    it(`should return ["Животные"] if offers have same category`, async () => {
+      const mockOffer1 = {
+        id: 'k2MJRx',
+        category: ['Животные'],
+        description: 'Если найдёте дешевле — сброшу цену. Бонусом отдам все аксессуары. Даю недельную гарантию. Продаю с болью в сердце...',
+        picture: 'item12.jpg',
+        title: 'Продам отличную подборку фильмов на VHS',
+        type: 'sale',
+        sum: 25913,
+        comments: [],
+      };
+      const mockOffer2 = {
+        id: 'bDBSaq',
+        category: ['Животные'],
+        description: 'Две страницы заляпаны свежим кофе. Даю недельную гарантию. Если найдёте дешевле — сброшу цену.',
+        picture: 'item13.jpg',
+        title: 'Подам отличную подборку фильмов на VHS',
+        type: 'offer',
+        sum: 31230,
+        comments: [],
+      };
+      const mockOffers = [mockOffer1, mockOffer2];
+      const server = await createServer(mockOffers);
+
+      const res = await request(server).get(`/api/categories`);
+
+      expect(res.body).toEqual(['Животные']);
+    });
+  });
 });
 
 
