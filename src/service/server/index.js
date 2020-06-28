22f0f2db766013707cb3d935744700636e33940b
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-06-28 15:13:55 +0400

    Добавляет логирование метода запроса

--- src/service/server/index.js
+++ src/service/server/index.js
@@ -33,7 +33,7 @@ const createServer = async ({offers, logger = pinoLogger} = {}) => {
   const router = await createRouter({offerService, commentService, categoryService, logger});
 
   server.use((req, res, next) => {
-    logger.debug(`Start request to url: ${ req.url }`);
+    logger.debug(`Start ${ req.method } request to url: ${ req.url }`);
 
     return next();
   });
@@ -42,6 +42,8 @@ const createServer = async ({offers, logger = pinoLogger} = {}) => {
     next();
 
     if (res.headersSent) {
+      // res.statusCode будет всегда равен 200. Хотелось бы логировать актуальное значение.
+      //  Если ли какие то способы, тут получить актуальный объект response?
       return logger.info(`End request with status code ${ res.statusCode }`);
     }
 
