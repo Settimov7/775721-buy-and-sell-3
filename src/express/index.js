b08d7d5c951f587e243cbdcb81bd1e02142103e5
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-08-02 13:32:29 +0400

    Добавляет отображение ошибок в консоль на фронт сервере

--- src/express/index.js
+++ src/express/index.js
@@ -28,6 +28,10 @@ app.use(`/my`, myRouter);
 app.use(`/offers`, offersRouter);
 app.use((req, res) => res.status(HttpStatusCode.NOT_FOUND).render(`errors/404`));
 // eslint-disable-next-line
-app.use((error, req, res, next) => res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).render(`errors/500`));
+app.use((error, req, res, next) => {
+  console.error(chalk.red(error));
+
+  res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).render(`errors/500`);
+});
 
 app.listen(DEFAULT_PORT, () => console.info(chalk.green(`Принимаю подключения на ${ DEFAULT_PORT }`)));
